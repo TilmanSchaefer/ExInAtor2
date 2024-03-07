8de614a2fff011e681cead547abde2d8b2915203
Tilman Schaefer <tilman.schaefer@gmail.com>
2024-03-07 10:43:29 +0000

    Testing different slicing of input data

--- recurrence_script_main.py
+++ recurrence_script_main.py
@@ -522,28 +522,14 @@ if not os.path.isfile('table_probabilities_R.txt'):
 		axx=numpy.random.randint(1,tot_len+1,tot_mut).tolist()
 		ex_simulated_count= len([x for x in range(len(axx)) if axx[x] <= ex_len])
 		return ex_simulated_count
-	def simulate(list2):
+	def simulate(list3):
 		numpy.random.seed(seed_number)
+		lines_to_print=[]
 		atime=time.time()
-		sets=0
-		gene=list2[0]
-		list2=list2[1]
-		results=[0] * simulations
-		for list in list2:
-			motif=list[0]
-			tot_mut=list[1]
-			tot_len=list[2]
-			ex_len=list[3]
-			result=[]
-			list=[[tot_mut,tot_len,ex_len]]*simulations
-			result=map(f2,list)
-			results=[x + y for x, y in zip(results, result)]
-		simulated_exonic_mutations=results
-		value1=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
-		del simulated_exonic_mutations
-		pval=(value1)/float(simulations)
-		if pval<=0.1:
-			sets+=1
+		for list2 in list3:
+			sets=0
+			gene=list2[0]
+			list2=list2[1]
 			results=[0] * simulations
 			for list in list2:
 				motif=list[0]
@@ -555,48 +541,66 @@ if not os.path.isfile('table_probabilities_R.txt'):
 				result=map(f2,list)
 				results=[x + y for x, y in zip(results, result)]
 			simulated_exonic_mutations=results
-			value2=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])+value1
+			value1=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
 			del simulated_exonic_mutations
-			pval=(value2)/float(simulations*2)
-		if pval<=0.01:
-			sets+=1
-			for i in range(10):
-				results=[0] * simulations*10
+			pval=(value1)/float(simulations)
+			if pval<=0.1:
+				sets+=1
+				results=[0] * simulations
 				for list in list2:
 					motif=list[0]
 					tot_mut=list[1]
 					tot_len=list[2]
 					ex_len=list[3]
 					result=[]
-					list=[[tot_mut,tot_len,ex_len]]*simulations*10
+					list=[[tot_mut,tot_len,ex_len]]*simulations
 					result=map(f2,list)
 					results=[x + y for x, y in zip(results, result)]
 				simulated_exonic_mutations=results
-				value2+=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
+				value2=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])+value1
 				del simulated_exonic_mutations
-			value3=value2
-			pval=(value3)/float(simulations*102)
-		if pval<=0.001:
-			sets+=1
-			for i in range(100):
-				results=[0] * simulations*100
-				for list in list2:
-					motif=list[0]
-					tot_mut=list[1]
-					tot_len=list[2]
-					ex_len=list[3]
-					result=[]
-					list=[[tot_mut,tot_len,ex_len]]*simulations*100
-					result=map(f2,list)
-					results=[x + y for x, y in zip(results, result)]
-				simulated_exonic_mutations=results
-				value2+=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
-				del simulated_exonic_mutations
-			value3=value2
-			pval=(value3)/float(simulations*10102)
-		line_to_print=gene+"\t"+str(real_exonic_mutations[gene])+"\t"+str(pval)+"\n"
-		#if sets>1: print(gene, sets, time.time()-atime, len(list2), pval)
-		return(line_to_print)
+				pval=(value2)/float(simulations*2)
+			if pval<=0.01:
+				sets+=1
+				for i in range(10):
+					results=[0] * simulations*10
+					for list in list2:
+						motif=list[0]
+						tot_mut=list[1]
+						tot_len=list[2]
+						ex_len=list[3]
+						result=[]
+						list=[[tot_mut,tot_len,ex_len]]*simulations*10
+						result=map(f2,list)
+						results=[x + y for x, y in zip(results, result)]
+					simulated_exonic_mutations=results
+					value2+=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
+					del simulated_exonic_mutations
+				value3=value2
+				pval=(value3)/float(simulations*102)
+			if pval<=0.001:
+				sets+=1
+				for i in range(100):
+					results=[0] * simulations*100
+					for list in list2:
+						motif=list[0]
+						tot_mut=list[1]
+						tot_len=list[2]
+						ex_len=list[3]
+						result=[]
+						list=[[tot_mut,tot_len,ex_len]]*simulations*100
+						result=map(f2,list)
+						results=[x + y for x, y in zip(results, result)]
+					simulated_exonic_mutations=results
+					value2+=len([i for i in simulated_exonic_mutations if i >= real_exonic_mutations[gene]])
+					del simulated_exonic_mutations
+				value3=value2
+				pval=(value3)/float(simulations*10102)
+			line_to_print=gene+"\t"+str(real_exonic_mutations[gene])+"\t"+str(pval)+"\n"
+		lines_to_print.append(line_to_print)
+			#if sets>1: print(gene, sets, time.time()-atime, len(list2), pval)
+		return(lines_to_print)
+
 
 	# Start simulation
 	def simulated_second_round(a):
@@ -617,8 +621,16 @@ if not os.path.isfile('table_probabilities_R.txt'):
 
 	start_time = time.time()
 	file2=open("table_probabilities_R.txt","w")
+
+	slice, rem = divmod(len(input_file), cores)
+	slices= numpy.multiply([s for s in range(0,cores+1)],slice)
+	slices[cores] += rem
+	sliced_input = []
+	for n in range(0,cores):
+		sliced_input.append(input_file[slices[n]:slices[n+1]])
+
 	pool = mp.Pool(processes=cores)
-	results = pool.map(simulate, input_file)
+	results = pool.map(simulate, sliced_input)
 	for element in results:
 		file2.write(element)
 	print("\tSimulation finished: %.0f seconds " % (time.time() - start_time))                                                                                            
